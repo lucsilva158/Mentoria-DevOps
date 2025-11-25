a725fc1a345a2f6f865c5049b4382c64931139a3
Lucas Oliveira Silva <lucas.osilva@linx.com.br>
2025-11-25 17:49:06 -0300

    Fix import in tests

--- testes/test_main.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-import Projeto
-
-client = TestClient(Projeto.app)
-
-def test_root():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
